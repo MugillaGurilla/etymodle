709e44d5cc9a6f45a5c438492f269bc6d0d56ea6
Cormac Porter <106176294+MugillaGurilla@users.noreply.github.com>
2025-10-27 20:16:48 +0000

    feat: using vetted answers from long term storage (#6)
    * feat: infrastructure for a long term storage approach to daily word generation
    
    * feat: long term storage for game setup data part 1
    
    * feat: long term storage for game setup data part 2
    
    * feat: long term storage for game setup data part 3
    
    * feat: long term storage for game setup data part 4
    
    * feat: long term storage for game setup data part 5
    
    * feat: updating today word generation to use vetted data from long term storage

---
+++ src/scripts/new-word-into-lts-libre.ts
@@ -0,0 +1,127 @@
+import * as fs from "fs";
+import { supportedLanguages } from "../../data/supported-languages.js";
+import { randomElement, sentencecase } from "../helpers/helpers.js";
+
+class NewWord {
+    private untranslated? : string;
+    private translated? : string;
+    private languageCode? : string;
+    private language? : string;
+    private milestones : Record<string, boolean> = {};
+
+    public async init() : Promise<void> {
+        this.fetchLanguage();
+        await this.generateWord();
+        await this.translate();
+        this.writeData();
+    }
+
+    private async generateWord() : Promise<void> {
+        if (!this.milestones.languageFetched) {
+            throw new Error("Language not fetched yet.");
+        }
+
+        try {
+            const raw = await fetch(`https://raw.githubusercontent.com/eymenefealtun/all-words-in-all-languages/refs/heads/main/${this.language}/${this.language}.txt`)
+            const all = (await raw.text()).split(",");
+            this.untranslated = randomElement(all);
+            this.milestones.wordGenerated = true;
+        } catch (error) {
+            throw new Error("Failed to fetch word list or generate word.");
+        };
+    }
+
+    private async translate() : Promise<void> {
+        if (!this.milestones.wordGenerated) {
+            throw new Error("Word not generated yet.");
+        }
+
+        const targetLanguage = "en"; 
+        const sourceLanguage = "auto";
+        const url = "http://localhost:5000/translate";
+        const method = "POST"
+        const body = JSON.stringify({
+            q: this.untranslated,
+            source: sourceLanguage,
+            target: targetLanguage,
+        });
+        const headers = { "Content-Type": "application/json" };
+
+        try {
+            const response = await fetch(url, {
+                method: method,
+                body: body,
+                headers: headers
+            });
+            if (!response.ok) {
+                throw new Error(`LibreTranslation API error: ${response.statusText}`);
+            }
+            const unparsed = await response.json();
+            const parsed = JSON.parse(unparsed)
+
+            this.translated = parsed.translatedText;
+            // this.languageCode = data[0][1];
+            this.milestones.wordTranslated = true;
+        }
+        catch (error) {
+            throw new Error("Failed to fetch meaning from translation API: " + error);
+        }
+    };
+
+    private fetchLanguage() : void {
+        if (supportedLanguages.length === 0) {
+            throw new Error("No supported languages available.");
+        }
+        
+        if (!supportedLanguages) {
+            throw new Error("Unavailable to fetch supported languages.");
+        }
+
+        this.language = sentencecase(randomElement(supportedLanguages));
+        this.milestones.languageFetched = true;
+    }
+
+    private writeData() : void {
+        if (!this.milestones.wordTranslated) {
+            throw new Error("Word not translated yet.");
+        }
+        console.log("Untranslated Word:", this.untranslated);
+        console.log("Translated Word:", this.translated);
+        console.log("Language Code:", this.languageCode);
+        console.log("Language:", this.language);
+
+        const data = {
+            untranslated: this.untranslated,
+            translated: this.translated,
+            language: this.language,
+            languageCode: this.languageCode,
+        }
+
+        let last : number = 1;
+        fs.readdir("./answer/all/", (err, files) => {
+            if (!files) {
+                console.error("Could not read './answer/all directory");
+            }; 
+            let biggest = 1;
+            files.map((file) => {
+                const current = parseInt(file.split(".")[0]);
+                if (current > biggest) {
+                    biggest = current;
+                }
+            });
+            fs.writeFile(`./answer/all/${biggest + 1}.json`, JSON.stringify(data, null, 2), (err) => {
+                if (err) {
+                    console.error("Error writing to file:", err);
+                } else {
+                    console.log(`Data successfully written to ${biggest + 1}.json`);
+                }
+            });
+        });
+
+    }
+}
+
+(async () => {
+    const newWord = new NewWord();
+    await newWord.init();
+})();
